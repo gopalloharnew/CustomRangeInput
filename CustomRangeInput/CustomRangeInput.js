89d6e3fac4c08210156dc7e1a7a41a9b6adbc3de
Gopal <gopalloharnew@gmail.com>
2022-04-26 23:00:21 +0530

    Adding HTML

--- CustomRangeInput/CustomRangeInput.js
+++ CustomRangeInput/CustomRangeInput.js
@@ -3,5 +3,23 @@ export default class CustomRangeInput {
     this.inputElem = inputElem;
     this.inputElem.classList.add("range-input-element");
     console.log(this.inputElem);
+    createRangeInputHTML.call(this);
   }
+}
+
+function createRangeInputHTML() {
+  this.rangeWraper = createDivOfClass("range-wraper");
+  this.rangeTrack = createDivOfClass("range-track");
+  this.rangeWidth = createDivOfClass("range-width");
+  this.rangeDot = createDivOfClass("range-dot");
+  this.rangeTrack.append(this.rangeWidth);
+  this.rangeWraper.append(this.rangeTrack);
+  this.rangeWidth.append(this.rangeDot);
+  this.inputElem.append(this.rangeWraper);
+}
+
+function createDivOfClass(className) {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add(className);
+  return newDiv;
 }

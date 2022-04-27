cc6948ccce284c74200fb37588c66ffad84dc1ac
Gopal <gopalloharnew@gmail.com>
2022-04-27 18:07:01 +0530

    Working Range Input

--- CustomRangeInput/CustomRangeInput.js
+++ CustomRangeInput/CustomRangeInput.js
@@ -1,10 +1,23 @@
 export default class CustomRangeInput {
-  constructor(inputElem) {
+  constructor(inputElem, callback, options = {}) {
     this.inputElem = inputElem;
+    this.min = options.min || 0;
+    this.max = options.max || 100;
+    this.rangeValue = options.value || this.min;
+    this.returnInt = options.returnInt || true;
+    this.callback = callback || (() => {});
+
     this.inputElem.classList.add("range-input-element");
-    console.log(this.inputElem);
     createRangeInputHTML.call(this);
     eventListenersForInput.call(this);
+  }
+
+  get value() {
+    return this.rangeValue;
+  }
+
+  set value(value) {
+    this.rangeValue = value;
   }
 }
 
@@ -17,6 +30,11 @@ function createRangeInputHTML() {
   this.rangeWraper.append(this.rangeTrack);
   this.rangeWidth.append(this.rangeDot);
   this.inputElem.append(this.rangeWraper);
+  this.rangeWraper.style.setProperty(
+    "--range-track-covered",
+    parseFloat(((this.rangeValue - this.min) * 100) / (this.max - this.min)) +
+      "%"
+  );
 }
 
 function createDivOfClass(className) {
@@ -31,17 +49,28 @@ function eventListenersForInput() {
   function rangeAction(e) {
     if (e.x < this.rangeTrackRect.x) {
       this.rangeProgress = 0;
+      this.value = this.min;
     } else if (e.x > this.rangeTrackRect.x + this.rangeTrackRect.width) {
+      this.value = this.max;
       this.rangeProgress = 100;
     } else {
       this.rangeProgress = parseFloat(
         ((e.x - this.rangeTrackRect.x) * 100) / this.rangeTrackRect.width
       );
+
+      let currentValue =
+        ((this.max - this.min) * this.rangeProgress) / 100 + this.min;
+      if (this.returnInt) {
+        this.value = parseInt(currentValue);
+      } else {
+        this.value = parseFloat(currentValue);
+      }
     }
     this.rangeWidth.style.setProperty(
       "--range-track-covered",
       this.rangeProgress + "%"
     );
+    this.callback(this.value);
   }
 
   const rangeClick = (e) => {
--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 import CustomRangeInput from "./CustomRangeInput/CustomRangeInput.js";
 
 const myCustomRangeInput = document.querySelector(".my-custom-range-input");
-let newCustomRangeInput = new CustomRangeInput(myCustomRangeInput);
+// let newCustomRangeInput = new CustomRangeInput(myCustomRangeInput);
+
+function test(e) {
+  console.log(e);
+}
+
+let newCustomRangeInput = new CustomRangeInput(myCustomRangeInput, test);
